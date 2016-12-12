acd2d1cda9621f58e5eccccce47bc65798f2299a
odayoqab <oo14kd@brocku.ca>
2016-12-11 22:53:46 -0500

    Add files via upload
    final assignment

---
+++ Final_Assignment/js/signup.js
@@ -0,0 +1,11 @@
+
+			function myFunction() {
+			    var x, text;
+			   
+			    x = document.getElementById("email").value;
+
+			    if (x = "text") {
+			        text = "Thank you for signing up! You'll receive an email from us shortly.";
+			    }
+			    document.getElementById("thankyou").innerHTML = text;
+			}
